a71ad997b19fa0f99b8be32e1ee23c12e9505b68
Alice Medeiros da Silva <alice_silva4@estudante.sesisenai.org.br>
2024-03-20 21:25:00 -0300

    Exercício carro

---
+++ src/Carro/Carro.java
@@ -0,0 +1,12 @@
+package Carro;
+
+public class Carro {
+    String marca;
+    String modelo;
+    Integer ano;
+    public void exibirInformacoes(String marca, String modelo, Integer ano){
+        System.out.println("Marca:" + marca);
+        System.out.println("Modelo:" + modelo);
+        System.out.println("Ano:" + ano);
+    }
+}
